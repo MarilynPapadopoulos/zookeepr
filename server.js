9e21e63f0455a3091c727175cde7861bac01011d
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-04-28 13:00:41 -0400

    added api data to animals

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 const fs = require('fs');
 const path = require('path');
 
-const express = require('express');
+
 const { animals } = require('./data/animals');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true}));
 //parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -93,7 +97,19 @@ app.get('/api/animals', (req, res) => {
 app.get('api/animals/:id', (req,res) => {
     const result = findById(req.params.id, animals);
     res.json(result);
-})
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/animals' ,(req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
